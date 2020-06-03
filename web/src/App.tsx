671f8c7f5aaa893528ceaa7d04d8b6fd45fbd1e5
Mirian Magri <magri.mirian@gmail.com>
2020-06-03 15:59:00 -0400

    including images, styles and pages home and collect point

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Routes from './routes';
 
 function App() {
   return (
-    <div>
-      <Header title="Hello World"/>
-      <h1>Application content</h1>
-    </div>
+   <Routes />
   );
 }
 
