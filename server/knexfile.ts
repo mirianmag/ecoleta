7b7e9e57a0ab962f45eab39e2fd5333df292a7ba
Mirian Magri <magri.mirian@gmail.com>
2020-06-03 09:59:35 -0400

    adding image by seed

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -5,9 +5,11 @@ module.exports = {
     connection: {
         filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
     },
-
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    },
+    seeds: {
+        directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
     },
     useNullAsDefault: true,
 };
